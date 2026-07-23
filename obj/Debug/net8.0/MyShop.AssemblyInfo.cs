b24b466f98b512d6cf2d0ba6cb754e00f86b1ce7
nguyenanhtuan771333-a11y <nguyenanhtuan771333@gmail.com>
2026-07-23 21:02:54 +0700

    Full rebuild after cleaning bin/obj

--- obj/Debug/net8.0/MyShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41570c9f597a41f0031cb305b87b7299748156b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eb1ef63365e2fc94644c4bdc628c86be88fa89b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
